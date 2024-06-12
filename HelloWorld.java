7e38becc1e35c9b111f0b88b8f565f173854e237
Ubuntu <ubuntu@ip-172-31-13-222.us-east-2.compute.internal>
2024-06-12 07:21:45 +0000

    committing to the feature branch

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,7 @@ class HelloWorld{
 	public static void main(String [] args){
 		System.out.println("Hello World");
 		System.out.println("DevOps Engineers");
+		System.out.println("DevOps Engineers, this was modied in the feature1 branch -- GIT");
 
 		return;
 	}
